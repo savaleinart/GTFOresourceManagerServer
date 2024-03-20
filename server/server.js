6c696ffc0019cbf761e77230a2ed476aea9a273b
Savaleinart <savaleinart@gmail.com>
2024-03-20 17:06:01 +0100

    remove extra http connection

--- server/server.js
+++ server/server.js
@@ -1,22 +1,3 @@
-
-const http = require('http');
-
-const hostname = '0.0.0.0';
-const port = 10000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`listening to port ${port}`)
-});
-
-const WebSocket = require("ws")
-
-
 const wss = new WebSocket.Server({port: 10101})
 
 let dataString = null
